3ed69d292b5c7c4c24505e76372d3cfc2162cabb
akhil <akhileshreddy9181@gmail.com>
2023-07-08 12:15:11 -0400

    Added Exit Status to script

--- vars.tf
+++ vars.tf
@@ -3,13 +3,13 @@ variable "folders" {
 }
 variable "jobs" {
   default = [
-    { name = "roboshop", folder = "Infra", repo_url = "https:github.com/akhileshreddy9181/roboshop-infra" },
-    { name = "frontend", folder = "CI-Pipeline", repo_url = "https:github.com/akhileshreddy9181/frontend" },
-    { name = "cart", folder = "CI-Pipeline", repo_url = "https:github.com/akhileshreddy9181/cart" },
+    { name = "roboshop", folder = "Infra", repo_url = "https://github.com/akhileshreddy9181/roboshop-infra" },
+    { name = "frontend", folder = "CI-Pipeline", repo_url = "https://github.com/akhileshreddy9181/frontend" },
+    { name = "cart", folder = "CI-Pipeline", repo_url = "https://github.com/akhileshreddy9181/cart" },
     { name = "catalogue", folder = "CI-Pipeline", repo_url = "https://github.com/akhileshreddy9181/catalogue.git" },
-    { name = "user", folder = "CI-Pipeline", repo_url = "https:github.com/akhileshreddy9181/user" },
-    { name = "shipping", folder = "CI-Pipeline", repo_url = "https:github.com/akhileshreddy9181/shipping" },
-    { name = "payment", folder = "CI-Pipeline", repo_url = "https:github.com/akhileshreddy9181/payment" }
+    { name = "user", folder = "CI-Pipeline", repo_url = "https://github.com/akhileshreddy9181/user" },
+    { name = "shipping", folder = "CI-Pipeline", repo_url = "https://github.com/akhileshreddy9181/shipping" },
+    { name = "payment", folder = "CI-Pipeline", repo_url = "https://github.com/akhileshreddy9181/payment" }
   ]
 
 }
