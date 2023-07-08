4f9777d46e7636e570a20d442e5eb19dee0d24c8
akhil <akhileshreddy9181@gmail.com>
2023-07-08 12:09:59 -0400

    Added Exit Status to script

--- vars.tf
+++ vars.tf
@@ -6,7 +6,7 @@ variable "jobs" {
     { name = "roboshop", folder = "Infra", repo_url = "https:github.com/akhileshreddy9181/roboshop-infra" },
     { name = "frontend", folder = "CI-Pipeline", repo_url = "https:github.com/akhileshreddy9181/frontend" },
     { name = "cart", folder = "CI-Pipeline", repo_url = "https:github.com/akhileshreddy9181/cart" },
-    { name = "catalogue", folder = "CI-Pipeline", repo_url = "github.com/akhileshreddy9181/catalogue" },
+    { name = "catalogue", folder = "CI-Pipeline", repo_url = "https://github.com/akhileshreddy9181/catalogue.git" },
     { name = "user", folder = "CI-Pipeline", repo_url = "https:github.com/akhileshreddy9181/user" },
     { name = "shipping", folder = "CI-Pipeline", repo_url = "https:github.com/akhileshreddy9181/shipping" },
     { name = "payment", folder = "CI-Pipeline", repo_url = "https:github.com/akhileshreddy9181/payment" }
