1875d4a94c8321db2c4a47ebde9f2c0d8928373c
akhil <akhileshreddy9181@gmail.com>
2023-07-09 10:50:45 -0400

    Added Exit Status to script

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "jenkins" {
 
-  server_url = "http://jenkins.devopsa.online:8080"
+  server_url = "http://172.31.2.183:8080"
   username   = data.aws_ssm_parameter.jenkins_user.value
   password   = data.aws_ssm_parameter.jenkins_pass.value
 }
